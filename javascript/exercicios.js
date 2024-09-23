9deb007ed356f6eb6af47b2fafb420988417cdb5
Guilherme Barros JR <dida0982@gmail.com>
2024-09-23 11:47:46 -0300

    troquei a function

--- javascript/exercicios.js
+++ javascript/exercicios.js
@@ -1,12 +1,12 @@
-function multiplicar(a, b) {
-  let resultado = a * b;
-  return resultado;
-}
-// multiplicar(5, 2);
-console.log(multiplicar(5,2));
+// function multiplicar(a, b) {
+//   let resultado = a * b;
+//   return resultado;
+// }
+// // multiplicar(5, 2);
+// console.log(multiplicar(5,2));
 
 
-document.querySelector("html").addEventListener("click", function () {
-    alert("Ai! Pare de me cutucar!");
-  });
+// document.querySelector("html").addEventListener("click", function () {
+//     alert("Ai! Pare de me cutucar!");
+//   });
   
